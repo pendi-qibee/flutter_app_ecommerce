e53678cc7bc1912d769785d7ea27ca2068a93620
pendimadyana <pendimadyana@gmail.com>
2020-10-15 07:17:42 +0700

    add constants and size config

---
+++ lib/size_config.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class SizeConfig {
+  static MediaQueryData _mediaQueryData;
+  static double screenWidth;
+  static double screenHeight;
+  static double defaultSize;
+  static Orientation orientation;
+
+  void init(BuildContext context) {
+    _mediaQueryData = MediaQuery.of(context);
+    screenWidth = _mediaQueryData.size.width;
+    screenHeight = _mediaQueryData.size.height;
+    orientation = _mediaQueryData.orientation;
+  }
+}
+
+double getProportionateScreenHeight(double inputHeight) {
+  double screenHeight = SizeConfig.screenHeight;
+  // 812 is the layout height from designer
+  return (inputHeight / 812.0) * screenHeight;
+}
+
+double getProportionateScreenWidth(double inputWidth) {
+  double screenWidth = SizeConfig.screenWidth;
+  // 375 is the layout height from designer
+  return (inputWidth / 375.0) * screenWidth;
+}
+
+// Reference
+//
+// Media Query
+// https://api.flutter.dev/flutter/widgets/MediaQuery-class.html
+// press CTRL + B to open in browser
